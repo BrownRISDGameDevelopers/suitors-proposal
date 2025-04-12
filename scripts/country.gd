bd31f331562b2b604f1cddd4b60c048de3bfc965
liteless <65517797+liteless@users.noreply.github.com>
2025-04-12 17:22:40 -0400

    add alpha bitmask to kingdoms

--- scripts/country.gd
+++ scripts/country.gd
@@ -1,4 +1,4 @@
-extends TextureRect
+extends TextureButton
 
 @export var kingdom: Kingdom = null
 @onready var popup: CountryPopup = $"../Popup"
@@ -6,7 +6,10 @@ extends TextureRect
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	popup.hide()
-
+	var image = texture_normal.get_image()
+	var bitmap = BitMap.new()
+	bitmap.create_from_image_alpha(image)
+	texture_click_mask = bitmap
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,7 +18,6 @@ func _process(delta: float) -> void:
 func _on_mouse_entered() -> void:
 	popup.show()
 	popup.kingdom = kingdom
-
 
 func _on_mouse_exited() -> void:
 	popup.hide()
