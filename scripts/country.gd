33eeaab21db6ed3fada20fdd961b30b38532d1b5
liteless <65517797+liteless@users.noreply.github.com>
2025-04-19 17:22:28 -0400

    tween map + desk setup

--- scripts/country.gd
+++ scripts/country.gd
@@ -4,16 +4,22 @@ extends TextureButton
 @onready var popup: CountryPopup = $"../Popup"
 
 const OUTLINE = preload("res://assets/shaders/outline.tres")
-const BORDER_OFFSET = Vector2(3, 3)
+const BORDER_OFFSET = Vector2(2, 2)
+
+var base_position: Vector2
+var current_tween
 #@onready var shader_mat := material as ShaderMaterial
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	popup.hide()
+	base_position = position
+	
 	var image = texture_normal.get_image()
 	var bitmap = BitMap.new()
 	bitmap.create_from_image_alpha(image)
 	texture_click_mask = bitmap
+	
 	material = OUTLINE.duplicate()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,15 +31,19 @@ func _on_mouse_entered() -> void:
 	popup.kingdom = kingdom
 
 	z_index = 2
-	position -= BORDER_OFFSET
+	
+	current_tween = create_tween()
+	current_tween.tween_property(self, "position", base_position - BORDER_OFFSET, 0.1)
+	
 	material.set_shader_parameter("enabled", true)
 	
 	
 func _on_mouse_exited() -> void:
 	var tween = create_tween()
 	
-	position += BORDER_OFFSET
 	material.set_shader_parameter("enabled", false)
 	z_index = 1
-	
 	popup.hide()
+	
+	current_tween = create_tween()
+	current_tween.tween_property(self, "position", base_position + BORDER_OFFSET, 0.1)
---
+++ scripts/resources/desk_interactable.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+class_name DeskInteractable
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var image = texture_normal.get_image()
+	var bitmap = BitMap.new()
+	bitmap.create_from_image_alpha(image)
+	texture_click_mask = bitmap
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
