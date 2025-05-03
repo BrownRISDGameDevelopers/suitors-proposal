9b1c0379ea1de50599af14e8973e331b86f26c35
Michael Rehmet <mrehmet@Michaels-MacBook-Pro-27.local>
2025-05-03 17:45:27 -0400

    theoretically added functionality that allows for ending

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,26 +1,32 @@
 extends TextureRect
 signal suitor_chosen(suitor)
 
-func _on_knight_dwarf_pressed() -> void:
-	_suitor_chosen(1)
-func _on_councilwoman_dwarf_pressed() -> void:
-	_suitor_chosen(1)
-func _on_bird_guy_pressed() -> void:
-	_suitor_chosen(1)
-func _on_peacock_pressed() -> void:
-	_suitor_chosen(1)
-func _on_mushroom_pressed() -> void:
-	_suitor_chosen(1)
-func _on_forest_lady_pressed() -> void:
-	_suitor_chosen(1)
-func _on_vampire_pressed() -> void:
-	_suitor_chosen(1)
-func _on_skeleton_pressed() -> void:
-	_suitor_chosen(1)
-func _on_water_guy_pressed() -> void:
-	_suitor_chosen(1)
-func _on_water_girl_pressed() -> void:
-	_suitor_chosen(1)
+@export var mountains: Kingdom = null
+@export var cloud: Kingdom = null
+@export var forest: Kingdom = null
+@export var water: Kingdom = null
+@export var undead: Kingdom = null
 
-func _suitor_chosen(suitor):
-	suitor_chosen.emit(suitor)
+func _on_knight_dwarf_pressed() -> void:
+	_suitor_chosen(1, mountains)
+func _on_councilwoman_dwarf_pressed() -> void:
+	_suitor_chosen(2, mountains)
+func _on_bird_guy_pressed() -> void:
+	_suitor_chosen(1, cloud)
+func _on_peacock_pressed() -> void:
+	_suitor_chosen(2, cloud)
+func _on_mushroom_pressed() -> void:
+	_suitor_chosen(1, forest)
+func _on_forest_lady_pressed() -> void:
+	_suitor_chosen(2, forest)
+func _on_vampire_pressed() -> void:
+	_suitor_chosen(1, undead)
+func _on_skeleton_pressed() -> void:
+	_suitor_chosen(2, undead)
+func _on_water_guy_pressed() -> void:
+	_suitor_chosen(1, water)
+func _on_water_girl_pressed() -> void:
+	_suitor_chosen(2, water)
+
+func _suitor_chosen(suitor, kingdom):
+	suitor_chosen.emit(suitor, kingdom)
